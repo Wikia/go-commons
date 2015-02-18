5b4724370febf12590f553e3d29e02850da3c13a
ArturKlajnerok <a.klajnerok@gmail.com>
2015-02-18 22:29:24 +0100

    [PLATFORM-550] Simple base http api client

---
+++ apiclient/apiclient.go
@@ -0,0 +1,61 @@
+package apiclient
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+type Client struct {
+	httpClient *http.Client
+	BaseURL    *url.URL
+}
+
+func NewClient(baseURL string) (*Client, error) {
+	parsedURL, err := url.Parse(baseURL)
+	if err != nil {
+		return nil, err
+	}
+	client := &Client{httpClient: http.DefaultClient, BaseURL: parsedURL}
+	return client, nil
+}
+
+func (client *Client) Call(method, endpoint string, data url.Values) ([]byte, error) {
+	request, err := client.NewRequest(method, endpoint, data)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := client.httpClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func (client *Client) NewRequest(method, endpoint string, data url.Values) (*http.Request, error) {
+	endpointUrl, err := url.Parse(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	fullUrl := client.BaseURL.ResolveReference(endpointUrl)
+
+	request, err := http.NewRequest(method, fullUrl.String(), bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	request.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+
+	return request, nil
+}
