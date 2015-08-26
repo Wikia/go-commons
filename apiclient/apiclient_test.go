1a5283c44235df3b400f12d1804244f422dd47ad
Łukasz Harasimowicz <dev@harnash.eu>
2015-08-26 16:27:53 +0200

    Added more tests for ApiClient.

--- apiclient/apiclient_test.go
+++ apiclient/apiclient_test.go
@@ -37,7 +37,7 @@ func TestNewRequest(t *testing.T) {
 	assert.NotNil(t, err, "NewRequest bad URL no error")
 }
 
-func TestCall(t *testing.T) {
+func TestCallWithHeaders(t *testing.T) {
 	client, _ := NewClient(HeaderTest)
 
 	headers := map[string]string{
@@ -57,5 +57,32 @@ func TestCall(t *testing.T) {
 	assert.NotNil(t, data["Custom-Header"], "Custom-Header is missing")
 	assert.Equal(t, "foo", data["Custom-Header"].(string), "Custom-Header is invalid")
 	assert.NotNil(t, data["Another-Header"], "Another-Header is missing")
-	assert.Equal(t, "bar", data["Another-Header"].(string), "Another-Header is invalid or missing")
+	assert.Equal(t, "bar", data["Another-Header"].(string), "Another-Header is invalid")
+}
+
+func TestCallWithoutHeaders(t *testing.T) {
+	client, _ := NewClient(HeaderTest)
+
+	response, err := client.Call("GET", "", nil, nil)
+
+	assert.NoError(t, err, "Error getting Header response")
+
+	var f interface{}
+	err = json.NewDecoder(response.Body).Decode(&f)
+	assert.NoError(t, err, fmt.Sprintf("Error deserializing JSON: %#v", response.Body))
+
+	data := f.(map[string]interface{})
+	assert.Equal(t, 3, len(data), "Incorrect number of headers sent")
+
+	for k, v := range data {
+		fmt.Printf("%v: %v\n", k, v.(string))
+	}
+	assert.NotNil(t, data["Content-Type"], "Content-Type is missing")
+	assert.Equal(t, "application/x-www-form-urlencoded", data["Content-Type"].(string), "Content-Type is invalid")
+
+	assert.NotNil(t, data["User-Agent"], "User-Agent is missing")
+	assert.Equal(t, "Go-http-client/1.1", data["User-Agent"].(string), "User-Agent is invalid")
+
+	assert.NotNil(t, data["Host"], "Host is missing")
+	assert.Equal(t, "headers.jsontest.com", data["Host"].(string), "Host is invalid")
 }
