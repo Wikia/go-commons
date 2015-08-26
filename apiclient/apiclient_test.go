8914932423c7df8f61a55f9a66b4dafa6c4847fe
Łukasz Harasimowicz <dev@harnash.eu>
2015-08-26 16:10:03 +0200

    Added support for sending custom headers to ApiClient.

--- apiclient/apiclient_test.go
+++ apiclient/apiclient_test.go
@@ -1,6 +1,8 @@
 package apiclient
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -10,6 +12,7 @@ var (
 	Endpoint    = "test"
 	EndpointURL = BaseURL + "/" + Endpoint
 	BadEndpoint = ":"
+	HeaderTest  = "http://headers.jsontest.com/"
 )
 
 func TestNewClient(t *testing.T) {
@@ -32,4 +35,27 @@ func TestNewRequest(t *testing.T) {
 	_, err = client.NewRequest("GET", BadEndpoint, nil)
 
 	assert.NotNil(t, err, "NewRequest bad URL no error")
+}
+
+func TestCall(t *testing.T) {
+	client, _ := NewClient(HeaderTest)
+
+	headers := map[string]string{
+		"custom-header":  "foo",
+		"Another-Header": "bar",
+	}
+
+	response, err := client.Call("GET", "", nil, headers)
+
+	assert.NoError(t, err, "Error getting Header response")
+
+	var f interface{}
+	err = json.NewDecoder(response.Body).Decode(&f)
+	assert.NoError(t, err, fmt.Sprintf("Error deserializing JSON: %#v", response.Body))
+
+	data := f.(map[string]interface{})
+	assert.NotNil(t, data["Custom-Header"], "Custom-Header is missing")
+	assert.Equal(t, "foo", data["Custom-Header"].(string), "Custom-Header is invalid")
+	assert.NotNil(t, data["Another-Header"], "Another-Header is missing")
+	assert.Equal(t, "bar", data["Another-Header"].(string), "Another-Header is invalid or missing")
 }
