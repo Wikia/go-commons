d214643a58cb4f04068a5ad79771093e49f1b6fb
nmonterroso <nelson@wikia-inc.com>
2015-10-05 13:58:55 -0700

    removing generic Check functions from interface

--- phalanx/phalanx.go
+++ phalanx/phalanx.go
@@ -23,17 +23,15 @@ type PhalanxClient interface {
 	CheckEmail(email string) (bool, error)
 	CheckEmails(names ...string) (bool, error)
 	CheckEmailsConcurrent(names ...string) (bool, error)
-	Check(checkType, content string) (bool, error)
-	CheckMultiple(checkType string, content ...string) (bool, error)
-	CheckMultipleConcurrent(checkType string, content ...string) (bool, error)
 }
 
-type client struct {
+type phalanxClientImpl struct {
 	apiClient *apiclient.Client
 }
 
 type checkResult struct {
 	contentType string
+	content     string
 	ok          bool
 	err         error
 }
@@ -43,35 +41,35 @@ func NewClient(baseURL string) (PhalanxClient, error) {
 	if err != nil {
 		return nil, err
 	}
-	client := &client{apiClient: apiClient}
+	client := &phalanxClientImpl{apiClient: apiClient}
 	return client, nil
 }
 
-func (client *client) CheckName(name string) (bool, error) {
-	return client.Check(checkTypeName, name)
+func (client *phalanxClientImpl) CheckName(name string) (bool, error) {
+	return client.check(checkTypeName, name)
 }
 
-func (client *client) CheckNames(names ...string) (bool, error) {
-	return client.CheckMultiple(checkTypeName, names...)
+func (client *phalanxClientImpl) CheckNames(names ...string) (bool, error) {
+	return client.checkMultiple(checkTypeName, names...)
 }
 
-func (client *client) CheckNamesConcurrent(names ...string) (bool, error) {
-	return client.CheckMultipleConcurrent(checkTypeName, names...)
+func (client *phalanxClientImpl) CheckNamesConcurrent(names ...string) (bool, error) {
+	return client.checkMultipleConcurrent(checkTypeName, names...)
 }
 
-func (client *client) CheckEmail(email string) (bool, error) {
-	return client.Check(checkTypeEmail, email)
+func (client *phalanxClientImpl) CheckEmail(email string) (bool, error) {
+	return client.check(checkTypeEmail, email)
 }
 
-func (client *client) CheckEmails(names ...string) (bool, error) {
-	return client.CheckMultiple(checkTypeEmail, names...)
+func (client *phalanxClientImpl) CheckEmails(names ...string) (bool, error) {
+	return client.checkMultiple(checkTypeEmail, names...)
 }
 
-func (client *client) CheckEmailsConcurrent(names ...string) (bool, error) {
-	return client.CheckMultipleConcurrent(checkTypeEmail, names...)
+func (client *phalanxClientImpl) CheckEmailsConcurrent(names ...string) (bool, error) {
+	return client.checkMultipleConcurrent(checkTypeEmail, names...)
 }
 
-func (client *client) Check(checkType, content string) (bool, error) {
+func (client *phalanxClientImpl) check(checkType, content string) (bool, error) {
 	data := url.Values{}
 	data.Add(typeKey, checkType)
 	data.Add(contentKey, content)
@@ -93,9 +91,9 @@ func (client *client) Check(checkType, content string) (bool, error) {
 	return false, nil
 }
 
-func (client *client) CheckMultiple(checkType string, content ...string) (bool, error) {
+func (client *phalanxClientImpl) checkMultiple(checkType string, content ...string) (bool, error) {
 	for _, c := range content {
-		ok, err := client.Check(checkType, c)
+		ok, err := client.check(checkType, c)
 
 		if err != nil || !ok {
 			return ok, err
@@ -105,11 +103,11 @@ func (client *client) CheckMultiple(checkType string, content ...string) (bool, 
 	return true, nil
 }
 
-func (client *client) CheckMultipleConcurrent(checkType string, content ...string) (bool, error) {
-	channels := make([]<-chan *checkResult)
+func (client *phalanxClientImpl) checkMultipleConcurrent(checkType string, content ...string) (bool, error) {
+	channels := make([]<-chan *checkResult, 0)
 
 	for _, c := range content {
-		channels = append(channels, client.checkContentConcurrent(checkType, c))
+		channels = append(channels, client.checkConcurrent(checkType, c))
 	}
 
 	for check := range waitForConcurrentRequests(channels...) {
@@ -123,16 +121,17 @@ func (client *client) CheckMultipleConcurrent(checkType string, content ...strin
 	return true, nil
 }
 
-func (client *client) checkContentConcurrent(checkType string, content string) <-chan *checkResult {
+func (client *phalanxClientImpl) checkConcurrent(contentType string, content string) <-chan *checkResult {
 	out := make(chan *checkResult)
 
 	go func() {
 		defer close(out)
-		ok, err := client.Check(checkType, content)
+		ok, err := client.check(contentType, content)
 		out <- &checkResult{
-			ok:      ok,
-			err:     err,
-			content: content,
+			ok:          ok,
+			err:         err,
+			content:     content,
+			contentType: contentType,
 		}
 	}()
 
