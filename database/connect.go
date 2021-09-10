7e3e1beef1f354f87111af7f5a93922ff4610e9f
Łukasz Harasimowicz <dev@harnash.eu>
2021-09-10 19:19:34 +0200

    Added database connection helper (GoRM)

---
+++ database/connect.go
@@ -0,0 +1,54 @@
+package database
+
+import (
+	"time"
+
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	dblogger "gorm.io/gorm/logger"
+	"gorm.io/plugin/dbresolver"
+	"moul.io/zapgorm2"
+)
+
+func GetConnection(logger *zap.Logger, logLevel dblogger.LogLevel, sources, replicas []string, connMaxIdleTime, connMaxLifeTime time.Duration, maxIdleConns, maxOpenConns int) (*gorm.DB, error) {
+	dbLogger := zapgorm2.New(logger)
+	dbLogger.SetAsDefault()
+
+	if len(sources) == 0 {
+		return nil, errors.New("no source database provided")
+	}
+
+	db, err := gorm.Open(mysql.Open(sources[0]), &gorm.Config{Logger: dbLogger.LogMode(logLevel)})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to connect database")
+	}
+
+	dbSources := make([]gorm.Dialector, len(sources))
+	for _, dsn := range sources {
+		dbSources = append(dbSources, mysql.Open(dsn))
+	}
+
+	dbReplicas := make([]gorm.Dialector, len(replicas))
+	for _, dsn := range replicas {
+		dbReplicas = append(dbReplicas, mysql.Open(dsn))
+	}
+
+	err = db.Use(
+		dbresolver.Register(dbresolver.Config{
+			Sources:  dbSources,
+			Replicas: dbReplicas,
+			Policy:   dbresolver.RandomPolicy{},
+		}).
+			SetConnMaxIdleTime(connMaxIdleTime).
+			SetConnMaxLifetime(connMaxLifeTime).
+			SetMaxIdleConns(maxIdleConns).
+			SetMaxOpenConns(maxOpenConns))
+
+	if err != nil {
+		return nil, errors.Wrap(err, "could not setup db replicas")
+	}
+
+	return db, nil
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/Wikia/go-commons
 go 1.16
 
 require (
+	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
 	github.com/armon/go-metrics v0.3.3 // indirect
+	github.com/go-playground/validator/v10 v10.9.0
 	github.com/google/btree v1.0.0 // indirect
 	github.com/hashicorp/consul/api v1.8.1
 	github.com/hashicorp/go-hclog v0.12.2 // indirect
@@ -12,13 +14,20 @@ require (
 	github.com/hashicorp/go-sockaddr v1.0.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/influxdata/influxdb v0.8.8
+	github.com/labstack/echo/v4 v4.5.0
 	github.com/miekg/dns v1.1.29 // indirect
 	github.com/mitchellh/mapstructure v1.2.2 // indirect
 	github.com/opentracing/opentracing-go v1.2.0
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.7.0
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	github.com/uber/jaeger-client-go v2.29.1+incompatible
+	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
+	go.uber.org/zap v1.16.0
 	golang.org/x/net v0.0.0-20210520170846-37e1c6afe023
+	gorm.io/driver/mysql v1.1.2
+	gorm.io/gorm v1.21.12
+	gorm.io/plugin/dbresolver v1.1.0
+	moul.io/zapgorm2 v1.1.0
 )
